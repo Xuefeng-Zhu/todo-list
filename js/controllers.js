51a88101e6792d2a118774c849f6d90dac99f3a0
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-10-05 00:42:49 -0500

    add login and sign up page

--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,17 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+controller('LoginCtrl', ['$scope',
+    function($scope) {
+        $scope.signup = function() {
+            $('.modal')
+                .modal('show');
+        }
 
-  }])
-  .controller('MyCtrl2', [function() {
+    }
+])
+    .controller('MyCtrl2', [
+        function() {
 
-  }]);
+        }
+    ]);
